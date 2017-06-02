c8ef5d732b29a9accd90f853706de759769b2e83
Thomas Christensen <thomas.s.christensen101@gmail.com>
2017-06-02 09:17:43 +0200

    who knows..

--- data_matlab/discharge_curve/discharge.m
+++ data_matlab/discharge_curve/discharge.m
@@ -4,7 +4,7 @@ data = load('discharge_data');
 t=(1:size(data,1)-1).*60;
 
 figure('position',[100 100 1200 400]);
-plot(t(1:39),data(2:40,1),t(1:51),data(2:52,2),t(1:end),data(2:end,3));
+plot(t(1:39),data(2:40,1),t(1:51),data(2:52,2),t(1:72),data(2:73,3),t,data(2:end,4));
 legend('Discharge Current - 4A','Discharge Current - 3A','Discharge Current - 2A','Discharge Current - 1A')
 title('Discharge Characteristics')
 xlabel('Time [S]')
